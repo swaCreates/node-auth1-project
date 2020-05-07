04d80b01f87d17263dc43cd00c42267b382f160d
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-05-07 12:15:17 -0400

    server file configured

--- api/server.js
+++ api/server.js
@@ -1,5 +1,10 @@
 const express= require('express');
 const helmet= require('helmet');
+const session= require('express-session');
+const KnexSessionStore= require('connect-session-knex')(session); // allows persistence of data to db even if server is restarted
+const db_config= require('../data/db_config.js'); // imported to put into session configuration
+const auth_router= require('../auth/auth-router.js');
+const user_router= require('../users/user-router.js');
 
 // creates instance of server
 const server= express();
@@ -7,8 +12,32 @@ const server= express();
 // built in & third party middleware
 server.use(express.json());
 server.use(helmet());
+// creating session object
+server.use(session({
+    name: 'token', // overwrites the default cookie, keeps our stack safe
+    resave: false, // avoids recreating sessions that have not changed
+    saveUninitialized: false, // GDPR laws, against setting cookies automatically
+    secret: 'keep it secret, keep it safe', // cryptographically sign the cookie
+    cookie: {
+        httpOnly: true, // disallows js from reading our cookie contents
+    },
+    store: new KnexSessionStore({
+        knex: db_config, // configured instance of knex
+        createtable: true, // if a sessions table doesn't exist, create one automatically
+    }),
+}));
 
-// routes
+// route handlers
+server.get('/', async (req, res, next) => {
+    try {
+        await res.send(`<h4 align='center'>Welcome to my server! :)</h4>`);
+    } catch (err) {
+        next(err);
+    };
+});
+
+server.use('/api', auth_router);
+server.use('/api/users', user_router);
 
 // handles no supporting route
 server.use((req, res) => {
@@ -19,7 +48,7 @@ server.use((req, res) => {
 
 // handles errors
 server.use((err, req, res, next) => {
-    console.log('Server error:', err)
+    console.log('Server error:', err);
 	res.status(500).json({
 		message: "Oops, something went wrong. Please try again later.",
 	});
---
+++ auth/auth-router.js
@@ -0,0 +1,13 @@
+const express= require('express');
+
+const router= express.Router();
+
+router.post('/register', async (req, res) => {
+
+});
+
+router.post('/login', async (req, res) => {
+
+});
+
+module.exports= router;
