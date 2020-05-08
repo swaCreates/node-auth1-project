b099b93fb8256bdedacac135d2166c8c4a928c98
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-05-08 12:20:04 -0400

    creating react frontend

---
+++ auth-users/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Users Database</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
